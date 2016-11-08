fa45ce557402d7348a4e8ad9c744e9d2a8d2faae
Martin Ehrnhoefer <martin@ehrnhoefer.com>
2016-11-08 21:12:07 +0100

    Deprecated classes removed

--- src/main/groovy/at/bxm/gradleplugins/svntools/SvnCheckout.groovy
+++
@@ -1,5 +0,0 @@
-package at.bxm.gradleplugins.svntools
-
-/** @deprecated use {@link at.bxm.gradleplugins.svntools.tasks.SvnCheckout} */
-@Deprecated
-class SvnCheckout extends at.bxm.gradleplugins.svntools.tasks.SvnCheckout {}
--- src/main/groovy/at/bxm/gradleplugins/svntools/SvnCommit.groovy
+++
@@ -1,5 +0,0 @@
-package at.bxm.gradleplugins.svntools
-
-/** @deprecated use {@link at.bxm.gradleplugins.svntools.tasks.SvnCommit} */
-@Deprecated
-class SvnCommit extends at.bxm.gradleplugins.svntools.tasks.SvnCommit {}
